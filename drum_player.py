3995508361cc9adc77886a2e61cf91ec775fab6d
Yeabkal <ywubshit@udallas.edu>
2020-04-08 18:40:52 -0500

    Add recording capabilities

--- drum_player.py
+++ drum_player.py
@@ -2,17 +2,31 @@
 import cv2
 import sound_player
 
-def main():
-    player = sound_player.SoundPlayer()
-    cap = cv2.VideoCapture(0)
+EXIT_KEY = 'q'
 
-    while(True):
-        _, frame = cap.read()
-        cv2.imshow('Frame', frame)
-        player.on_key_pressed(cv2.waitKey(1))
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+class DrumPlayer(object):
+    def __init__(self):
+        self.player = sound_player.SoundPlayer()
+    
+    def run(self):
+        cap = cv2.VideoCapture(0)
+
+        while(True):
+            _, frame = cap.read()
+            cv2.imshow('Frame', frame)
+
+            key = cv2.waitKey(1)
+            if key == ord(EXIT_KEY):
+                self.player.print_logs()
+                self.player.play_recording(play_from_first_note=True)
+                self.player.save_recording('test')
+                return
+            self.player.play_key(key)
+        
+        cap.release()
+        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main()
+    # drum_player = DrumPlayer()
+    # drum_player.run()
+    sound_player.SoundPlayer().play_file('recordings/test')
